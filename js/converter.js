d102e4e8cd89bad0b857b3ab6dc02727f8442bee
Adrián <arodriba@ull.edu.es>
2016-02-24 23:29:00 +0000

    Finished regular expression. Version 3

--- js/converter.js
+++ js/converter.js
@@ -3,28 +3,27 @@ function convert() {
     var result;
     var result2;
     var temp = original.value;
-    //var regexp = /([+-]?\d+(?:\.\d+)?(?:e[+-]?\d+)?)\s*([cfk])/i;
-    var regexp = /^([-+]?\d+(?:\.\d*)?(?:e[-+]?\d+)?)\s*((?:f(?:a(?:r(?:e(?:n(?:h(?:e(?:i(?:t)?)?)?)?)?)?)?)?)|(?:c(?:e(?:l(?:s(?:i(?:u(?:s)?)?)?)?)?)?)|(?:k(?:e(?:l(?:v(?:i(?:n)?)?)?)?)?))\s*$/i;
+    var regexp = (/^([-+]?\d+(?:\.\d*)?(?:e[-+]?\d+)?)\s*((?:[fF](?:[aA](?:[rR](?:[eE](?:[nN](?:[hH](?:[eE](?:[iI](?:[tT])?)?)?)?)?)?)?)?)|(?:[cC](?:[eE](?:[lL](?:[sS](?:[iI](?:[uU](?:[sS])?)?)?)?)?)?)|(?:[kK](?:[eE](?:[lL](?:[vV](?:[iI](?:[nN])?)?)?)?)?))\s*$/i);
     var m = temp.match(regexp);
 
     if (m) {
-        var num = m[1];
-        var type = m[2];
-        num = parseFloat(num);
-        if (type == 'c' || type == 'C') {
-            result = (num * 9 / 5) + 32;
+        var value = m[1];
+        var unit = m[2];
+        value = parseFloat(value);
+        if (unit.charAt(0) == 'C' || unit.charAt(0) == 'c') {
+            result = (value * 9 / 5) + 32;
             result = result.toFixed(1) + " Farenheit";
-            result2 = num + 273.15 + " Kelvin";
-        } else if (type == 'f' || type == 'F') {
-            result = (num - 32) * 5 / 9;
-            result2 = ((num + 459.67) + 5 / 9) + " Kelvin";
+            result2 = value + 273.15 + " Kelvin";
+        } else if (unit.charAt(0) == 'F' || unit.charAt(0) == 'f') {
+            result = (value - 32) * 5 / 9;
+            result2 = ((value + 459.67) + 5 / 9) + " Kelvin";
             result = result.toFixed(1) + " Celsius";
         } else {
-            result = (num - 273.15) + " Celsius";
-            result2 = ((num * 9 / 5) - 459.67) + " Farenheit";
+            result = (value - 273.15) + " Celsius";
+            result2 = ((value * 9 / 5) - 459.67) + " Farenheit";
         }
         converted.innerHTML = result + "<br/>" + result2;
     } else {
-        converted.innerHTML = "ERROR! Try something like '-4.2C' instead";
+        converted.innerHTML = "Invalid input! Please read the 'Usage' and 'Input Examples' sections.";
     }
 }
