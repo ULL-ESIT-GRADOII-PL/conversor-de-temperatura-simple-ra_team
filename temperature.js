0ce8ac5501c5881e0a10a83a8a6bb9f95dc4a2b1
Adrián Rodríguez Bazaga <alu0100826456@ull.edu.es>
2016-02-24 16:05:55 +0000

    New regular expression for the temperature

--- temperature.js
+++ temperature.js
@@ -2,7 +2,7 @@
 function calculate() {
   var result;
   var temp = original.value;
-  var regexp = /([-+]?\d+(?:\.\d*)?)\s*([fFcC])/;
+  var regexp = /([+-]?\d+(?:\.\d+)?(?:e[+-]?\d+)?)\s*([cf])/i;
   
   var m = temp.match(regexp);
   
