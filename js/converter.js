068e687431f00a0c42a56141241a7ca51cbb2843
Adrián Rodríguez Bazaga <alu0100826456@ull.edu.es>
2016-02-28 01:09:12 +0000

    Improved regexp

--- js/converter.js
+++ js/converter.js
@@ -3,7 +3,31 @@ function convert() {
     var result;
     var result2;
     var temp = original.value;
-    var regexp = (/^([-+]?\d+(?:\.\d*)?(?:e[-+]?\d+)?)\s*((?:[fF](?:[aA](?:[hH](?:[rR](?:[eE](?:[nN](?:[hH](?:[eE](?:[iI](?:[tT])?)?)?)?)?)?)?)?)?)|(?:[cC](?:[eE](?:[lL](?:[sS](?:[iI](?:[uU](?:[sS])?)?)?)?)?)?)|(?:[kK](?:[eE](?:[lL](?:[vV](?:[iI](?:[nN])?)?)?)?)?))\s*$/i);
+    var regexp = (\s*^([-+]?\d+(?:\.\d*)?(?:e[-+]?\d+)?)\s*((?:[fF](?:[aA](?:[rR](?:[eE](?:[nN](?:[hH](?:[eE](?:[iI](?:[tT])?)?)?)?)?)?)?)?)|(?:[cC](?:[eE](?:[lL](?:[sS](?:[iI](?:[uU](?:[sS])?)?)?)?)?)?)|(?:[kK](?:[eE](?:[lL](?:[vV](?:[iI](?:[nN])?)?)?)?)?))\s*$/i);
+    var m = temp.match(regexp);
+
+    if (m) {
+        var value = m[1];
+        var unit = m[2];
+        value = parseFloat(value);
+        if (unit.charAt(0) == 'C' || unit.charAt(0) == 'c') {
+            result = (value * 9 / 5) + 32;
+            result = result.toFixed(1) + " Farenheit";
+            result2 = value + 273.15 + " Kelvin";
+        } else if (unit.charAt(0) == 'F' || unit.charAt(0) == 'f') {
+            result = (value - 32) * 5 / 9;
+            result2 = ((value + 459.67) + 5 / 9) + " Kelvin";
+            result = result.toFixed(1) + " Celsius";
+        } else {
+            result = (value - 273.15) + " Celsius";
+            result2 = ((value * 9 / 5) - 459.67) + " Farenheit";
+        }
+        converted.innerHTML = result + "<br/>" + result2;
+    } else {
+        converted.innerHTML = "Invalid input! Please read the 'Usage' and 'Input Examples' sections.";
+    }
+}
+
     var m = temp.match(regexp);
 
     if (m) {
